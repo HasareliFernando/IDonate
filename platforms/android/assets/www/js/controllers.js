01e891c9b8b07dad539bd672b82ddcaf3aa3be72
HasareliFernando <fernandohasareli@gmail.com>
2017-05-08 09:25:27 +0530

    addplug

---
+++ platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,154 @@
+angular.module('app.controllers', [])
+  
+.controller('LogInCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('iDonateCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('iDonate2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('settingsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('timelineCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('bloodRequestCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('addPostCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('searchBloodBankCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('bloodCompatibilityCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('reportUserCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('addAsDonorCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+	$('#addAsDonor-button29').click(function(){
+			var addAsDonor-input31=$scope.addAsDonor-input31;
+			var input31=$('#addAsDonor-input31').val();
+			var datastring=addAsDonor-input31;
+			if(input31 == ''){
+				$('#response').html('Please enter your id');
+			}
+			else{
+				
+			}
+	});
+
+}])
+   
+.controller('addAsDonor2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('addAsDonor3Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ .controller('myProfileCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('viewCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('remindersCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('statusCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('editProfileCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
---
+++ platforms/android/assets/www/js/routes.js
@@ -0,0 +1,125 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('LogIn', {
+    url: '/page3',
+    templateUrl: 'templates/LogIn.html',
+    controller: 'LogInCtrl'
+  })
+
+  .state('iDonate', {
+    url: '/page4',
+    templateUrl: 'templates/iDonate.html',
+    controller: 'iDonateCtrl'
+  })
+
+  .state('iDonate2', {
+    url: '/page5',
+    templateUrl: 'templates/iDonate2.html',
+    controller: 'iDonate2Ctrl'
+  })
+
+  .state('settings', {
+    url: '/page6',
+    templateUrl: 'templates/settings.html',
+    controller: 'settingsCtrl'
+  })
+
+  .state('timeline', {
+    url: '/page7',
+    templateUrl: 'templates/timeline.html',
+    controller: 'timelineCtrl'
+  })
+
+  .state('bloodRequest', {
+    url: '/page8',
+    templateUrl: 'templates/bloodRequest.html',
+    controller: 'bloodRequestCtrl'
+  })
+
+  .state('addPost', {
+    url: '/page9',
+    templateUrl: 'templates/addPost.html',
+    controller: 'addPostCtrl'
+  })
+
+  .state('searchBloodBank', {
+    url: '/page10',
+    templateUrl: 'templates/searchBloodBank.html',
+    controller: 'searchBloodBankCtrl'
+  })
+
+  .state('bloodCompatibility', {
+    url: '/page11',
+    templateUrl: 'templates/bloodCompatibility.html',
+    controller: 'bloodCompatibilityCtrl'
+  })
+
+  .state('reportUser', {
+    url: '/page12',
+    templateUrl: 'templates/reportUser.html',
+    controller: 'reportUserCtrl'
+  })
+
+  .state('addAsDonor', {
+    url: '/page13',
+    templateUrl: 'templates/addAsDonor.html',
+    controller: 'addAsDonorCtrl'
+  })
+
+  .state('addAsDonor2', {
+    url: '/page14',
+    templateUrl: 'templates/addAsDonor2.html',
+    controller: 'addAsDonor2Ctrl'
+  })
+
+  .state('addAsDonor3', {
+    url: '/page15',
+    templateUrl: 'templates/addAsDonor3.html',
+    controller: 'addAsDonor3Ctrl'
+  })
+  
+      .state('myProfile', {
+    url: '/myProfile',
+    templateUrl: 'templates/myProfile.html',
+    controller: 'myProfileCtrl'
+  })
+
+  .state('view', {
+    url: '/view',
+    templateUrl: 'templates/view.html',
+    controller: 'viewCtrl'
+  })
+
+  .state('reminders', {
+    url: '/reminders',
+    templateUrl: 'templates/reminders.html',
+    controller: 'remindersCtrl'
+  })
+
+  .state('status', {
+    url: '/status',
+    templateUrl: 'templates/status.html',
+    controller: 'statusCtrl'
+  })
+
+  .state('editProfile', {
+    url: '/editProfile',
+    templateUrl: 'templates/editProfile.html',
+    controller: 'editProfileCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page3')
+
+  
+
+});
